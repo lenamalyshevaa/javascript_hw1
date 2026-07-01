bc6a20cb4ee5fc1257bef517c4e741640687e0fa
lenamalyshevaa <elenamalysheva904@gmail.com>
2026-07-01 16:06:51 +0400

    Update about_assignment.js

--- JS-Koans/topics/1 Введение в JavaScript/about_assignment.js
+++ JS-Koans/topics/1 Введение в JavaScript/about_assignment.js
@@ -1,4 +1,16 @@
-
+describe("About Assignment (about_assignment.js)", function() {
+  it("local variables", function() {
+    let temp = 1;
+    // Присвойте значение переменной temp
+    expect(temp).toBe(1);
+  });
+  
+  it("global variables", function() {
+    temp = 1; // Неиспользование let - это пример. На практике всегда используйте let.
+    // глобальные переменные присваиваются объекту window
+    expect(window.temp).toBe(temp);
+  });
+});
 describe("About Assignment (about_assignment.js)", function() {
   it("local variables", function() {
     let temp = FILL_ME_IN;
